70a37633c2132ee8586a890059173ef9cded93b5
Guido van de Riet <guidovdriet@gmail.com>
2019-06-25 17:31:12 +0200

    filter data results for undefined values

--- app/controllers/neighbourhoodController.js
+++ app/controllers/neighbourhoodController.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Neighbourhood = mongoose.model('Neighbourhood');
+const Neighbourhood = mongoose.model("Neighbourhood");
 
-var geo = require('mapbox-geocoding');
+var geo = require("mapbox-geocoding");
 
 geo.setAccessToken(
-  'pk.eyJ1IjoiZ3VpZG92ZHJpZXQiLCJhIjoiY2p3ZGZxdDh6MDdjYjQzcGIxZmRhZHdtdSJ9._CHi3885MJVa8AY9fsIgJw'
+  "pk.eyJ1IjoiZ3VpZG92ZHJpZXQiLCJhIjoiY2p3ZGZxdDh6MDdjYjQzcGIxZmRhZHdtdSJ9._CHi3885MJVa8AY9fsIgJw"
 );
 
 exports.getAll = async (req, res) => {
   const Neighbourhoods = await Neighbourhood.find();
   if (!Neighbourhoods) {
-    res.status(404).json({ error: 'Page not found' });
+    res.status(404).json({ error: "Page not found" });
   }
   res.status(200).json(Neighbourhoods);
 };
@@ -21,13 +21,20 @@ exports.getByData = async (req, res) => {
   const { month } = req.query;
   const { day } = req.query;
 
-  console.log({
-    query: req.query,
-    test: 'hjkdhkjd',
+  const obj = {
     year,
     month,
     day
-  });
+  };
+
+  // Filter all undefined values from object
+  const data = Object.keys(obj).reduce((acc, key) => {
+    const result = acc;
+    if (obj[key] !== undefined) result[key] = obj[key];
+    return result;
+  }, {});
+
+  console.log({ data });
 
   res.status(200).json();
 };
@@ -44,16 +51,16 @@ exports.getByWOZbySquareFeed = async (req, res) => {
     .limit(4);
 
   if (!WOZbySquareFeed) {
-    res.status(404).json({ error: 'Page not found' });
+    res.status(404).json({ error: "Page not found" });
   }
 
   res.status(200).json(WOZbySquareFeed);
 };
 
 exports.getHighestSatisfaction = async (req, res) => {
-  const prop = 'socialAverage';
-  const propOne = 'physicalAverage';
-  const propTwo = 'physicalAverage';
+  const prop = "socialAverage";
+  const propOne = "physicalAverage";
+  const propTwo = "physicalAverage";
 
   const priority = -1;
   const priorityOne = -1;
@@ -68,7 +75,7 @@ exports.getHighestSatisfaction = async (req, res) => {
     .limit(4);
 
   if (!neighbourhoodsHighestSatisfaction) {
-    res.status(404).json({ error: 'Page not found' });
+    res.status(404).json({ error: "Page not found" });
   }
 
   res.status(200).json(neighbourhoodsHighestSatisfaction);
